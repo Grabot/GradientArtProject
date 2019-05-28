e4fc9d60636807a17346c70800fbc220f44a3f3f
Grabot <SanderKools@gmail.com>
2019-05-28 23:01:21 +0200

    added the colour arrays with sobel calculations

--- GradientArt.py
+++ GradientArt.py
@@ -2,13 +2,33 @@ import cv2
 import numpy as np
 
 
-if __name__ == "__main__":
+def load_image_grey(image_name):
+    return cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
 
+
+def load_image_colour(image_name, colour):
+    c = []
+    image = cv2.imread(image_name)
+
+    for x in image:
+        line = []
+        for y in x:
+            line.append(y[colour])
+        c.append(line)
+
+    return c
+
+
+if __name__ == "__main__":
     # First we will test it with the grayscale.
-    img = cv2.imread('Lenna.png', cv2.IMREAD_GRAYSCALE)
+    img = load_image_grey('Lenna.png')
+
+    img_red = load_image_colour('Lenna.png', 0)
+    img_green = load_image_colour('Lenna.png', 1)
+    img_blue = load_image_colour('Lenna.png', 2)
     rows, cols = img.shape
 
-    image = np.sqrt(img)
+    image = np.sqrt(img_blue)
 
     gx = cv2.Sobel(np.float32(image), cv2.CV_32F, 1, 0, ksize=1)
     gy = cv2.Sobel(np.float32(image), cv2.CV_32F, 0, 1, ksize=1)
@@ -17,7 +37,6 @@ if __name__ == "__main__":
     cv2.imshow('Sobel vertical', gx)
     cv2.imshow('Sobel horizontal', gy)
 
-    print(gx)
-
+    # print(gx)
     mag, ang = cv2.cartToPolar(gx, gy)
     cv2.waitKey(0)
